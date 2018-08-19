60c4d837d8aa28eff89854a5b78bfff9bcb9f57e
kamran ullah <kamranullah@hotmail.com>
2018-08-20 00:19:27 +0100

    Sort with spaceship

--- index.php
+++ index.php
@@ -1,1 +1,65 @@
-"echo sprintf('Salam %s','World')" 
+<?php
+/*
+** Sorting by spcaeship operator
+*/
+
+class User
+{
+	protected $name;
+	protected $age;
+
+	public function __construct($name, $age)
+	{
+		$this->name=$name;
+		$this->age=$age;
+	}
+
+	public function name()
+	{
+		return $this->name;
+	}
+
+	public function age()
+	{
+		return $this->age;
+	}
+}
+
+
+class userCollection
+{
+
+	public $users; 
+	
+	public function __construct(array $users)
+	{
+		$this->users = $users;
+	}
+
+	public function users()
+	{
+		return $this->users;
+	}
+
+	public function sortBy($property)
+	{
+		
+		usort($this->users, function($userOne, $userTwo) use ($property)
+		{
+		
+			return $userTwo->$property() <=> $userOne->$property();
+		});
+	}
+
+}
+
+
+$users = new userCollection([
+						 		 new user('Grand dad',		75)
+								,new user('Son', 			44)
+								,new user('Grand mother',	71)
+								,new user('Grand son',   	 3)
+					]);
+
+$users->sortBy('age'); // or sortBy name
+print_r($users->users());
